3378c667b178cafc82f03bdfbf54bec0112d1d8b
James Heiniger <james.heiniger911@gmail.com>
2025-03-14 22:05:42 +1030

    Add files via upload
    Added base code files

---
+++ script.js
@@ -0,0 +1,10 @@
+let horoscope = ''
+horoscope ?
+let moonSign = Math.Floor(Math.random() * 13);
+let starSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Piscies']
+const star = () => {
+
+}
+const moon = () => {
+
+}
